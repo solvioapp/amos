1d01e0c5fda667c18cfe70c322569d074058bc6c
dteiml <dteiml@gmail.com>
2019-09-27 12:19:29 +0200

    add constants

--- api/src/schema/amos-game/resolvers.js
+++ api/src/schema/amos-game/resolvers.js
@@ -1,15 +1,8 @@
 import {fs,path} from '../../common'
-const _1 = fs.readFileSync (path.resolve (__dirname, `neo4j-query.cypher`), {encoding: `utf8`})
+const _1 = require (`neo4j-query.cypher`)
+
 const metascraper = require('metascraper')([
-  // require('metascraper-author')(),
-  // require('metascraper-date')(),
-  // require('metascraper-description')(),
-  // require('metascraper-image')(),
-  // require('metascraper-logo')(),
-  // require('metascraper-clearbit')(),
-  // require('metascraper-publisher')(),
-  require('metascraper-title')(),
-  // require('metascraper-url')()
+  require('metascraper-title')()
 ])
 
 const got = require('got')
@@ -23,15 +16,18 @@ const resolvers = {
     addReview: async (_, {input: {type, links, topics, prerequisites}}, {driver, ip}) => {
       // Assuming no other resource exists with at least one such link
       // Assuming reviews are anonymous
+
       topics |> console.log('topics', #)
       prerequisites |> console.log('prerequisites', #)
+      
       // TODO: generalize
       const ses = driver.session()
       // TODO: don't choose first link
-      const { body: html, url } = await got(links[0])
-      const {title} = await metascraper({ html, url })
+      const {body: html, url} = await got (links[0])
+      const {title} = await metascraper ({html, url})
 
       // TODO: also prerequisites
+      // TODO: topics
       await ses.run (_1, {ip, title, type, links, topics, prerequisites})
 
       return true
