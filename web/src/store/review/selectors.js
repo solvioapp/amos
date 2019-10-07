fb07c1d5109edf72331010637f21171a9dbc54b6
dteiml <dteiml@gmail.com>
2019-10-07 17:02:24 +0200

    after delete redux

--- web/src/store/review/selectors.js
+++
@@ -1,9 +0,0 @@
-import * as R from 'ramda'
-import {createSelector} from 'reselect'
-
-export const selectReview = R.prop(`review`)
-
-export const selectSlogan = createSelector(
-  selectReview,
-  R.prop(`slogan`)
-)
--- web/src/store/search/selectors.js
+++
@@ -1,9 +0,0 @@
-import * as R from 'ramda'
-import {createSelector} from 'reselect'
-
-export const selectSearch = R.prop(`search`)
-
-export const selectSlogan = createSelector(
-  selectSearch,
-  R.prop(`slogan`)
-)
