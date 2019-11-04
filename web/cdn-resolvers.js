102139dde6ff871773e17fc11e5a39ea1d841af9
dteiml <dteiml@gmail.com>
2019-11-04 08:28:25 +0100

    remove former cdn resolvers

--- web/cdn-resolvers.js
+++ web/cdn-resolvers.js
@@ -1,6 +1,6 @@
 const moduleToCdn = require (`module-to-cdn`)
 
-function resolve(name, version) {
+const resolve = (name, version) => {
   const list = [
     {
       name: `history`,
@@ -12,30 +12,6 @@ function resolve(name, version) {
       name: `ramda`,
       var: `R`,
       url: `https://unpkg.com/ramda@${version}/dist/ramda.min.js`,
-      version
-    },
-    {
-      name: `react-redux`,
-      var: `ReactRedux`,
-      url: `https://unpkg.com/react-redux@${version}/dist/react-redux.min.js`,
-      version
-    },
-    {
-      name: `redux-saga`,
-      var: `ReduxSaga`,
-      url: `https://unpkg.com/redux-saga@${version}/dist/redux-saga.umd.min.js`,
-      version
-    },
-    {
-      name: `regenerator-runtime`,
-      var: `regeneratorRuntime`,
-      url: `https://unpkg.com/regenerator-runtime@${version}/runtime.js`,
-      version
-    },
-    {
-      name: `reselect`,
-      var: `Reselect`,
-      url: `https://unpkg.com/reselect@${version}/dist/reselect.min.js`,
       version
     },
     {
